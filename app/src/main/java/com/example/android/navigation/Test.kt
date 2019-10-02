e2512c26b10ea4a75fd8d0d70959858b35a2f3da
shailendras <shailendra.sisodiya@viacom18.com>
2019-10-02 14:12:46 +0530

    Navigation library android

---
+++ app/src/main/java/com/example/android/navigation/Test.kt
@@ -0,0 +1,47 @@
+import java.io.*
+import java.math.*
+import java.security.*
+import java.text.*
+import java.util.*
+import java.util.concurrent.*
+import java.util.function.*
+import java.util.regex.*
+import java.util.stream.*
+import kotlin.collections.*
+import kotlin.comparisons.*
+import kotlin.io.*
+import kotlin.jvm.*
+import kotlin.jvm.functions.*
+import kotlin.jvm.internal.*
+import kotlin.ranges.*
+import kotlin.sequences.*
+import kotlin.text.*
+
+// Complete the jumpingOnClouds function below.
+fun jumpingOnClouds(c: Array<Int>): Int {
+    var safeJumpCount = 0
+    var counter = 0
+    while (counter < c.size){
+        counter += 2
+        if (c[counter] == 0){
+            safeJumpCount++
+        }else{
+            counter--
+            safeJumpCount++
+        }
+    }
+    return safeJumpCount
+
+}
+
+fun main(args: Array<String>) {
+    val scan = Scanner(System.`in`)
+
+    val n = scan.nextLine().trim().toInt()
+
+    val c = scan.nextLine().split(" ").map{ it.trim().toInt() }.toTypedArray()
+
+    val result = jumpingOnClouds(c)
+
+    println(result)
+}
